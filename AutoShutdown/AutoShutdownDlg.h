63d13970c03318ca2bef52c163b8e0de2af87a50
dorian-li <dorian.li@outlook.com>
2019-05-01 14:30:34 +0800

    添加项目文件。

---
+++ AutoShutdown/AutoShutdownDlg.h
@@ -0,0 +1,39 @@
+﻿
+// AutoShutdownDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CAutoShutdownDlg 对话框
+class CAutoShutdownDlg : public CDialogEx
+{
+// 构造
+public:
+	CAutoShutdownDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_AUTOSHUTDOWN_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnBnClickedCancel();
+	afx_msg void OnBnClickedStart();
+	afx_msg void OnBnClickedStop();
+};
